42013567d286d6b01cabeac1842b293ee4e0e3f4
siruliina <siruliinakarlin@hotmail.com>
2024-09-04 19:35:20 +0300

    AboutMe page continued

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,10 +1,36 @@
-// import '../css/Work.css';
+import '../css/AboutMe.css';
 
 function AboutMe() {
   return (
-    <p>
-        Here I will tell who I am.
-    </p>
+    <div className="grid-container">
+      <div>
+        <h4><b>Education</b></h4>
+        <p>
+          My name is Siru-Liina Karlin and I graduated in summer 2024 with a bachelor's degree in ICT. 
+          I studied in Turku University of Applied Sciences.
+          My study path in school was Software Engineering and Project Management.
+          The studies involved a lot of teamwork and project work. I was the project manager in some projects
+          and learned a lot about the importance of scheduling and thorough planning. I feel like my years as
+          a team member in a gymnastics team and me being a coach really helped me in a lot of tasks because I have learned
+          how to be an effective part of a team but also take care of others so that they can do their best.
+          I am interested especially in frontend coding and UI/UX design, however right now I'm very open to learning
+          new things and taking on new challenges. In school I used Figma a lot to plan
+          website layouts.
+        </p>
+      </div>
+      <div>
+        <h4><b>Personality</b></h4>
+        <p>
+          
+        </p>
+      </div>
+      <div>
+        <h4><b>Interests</b></h4>
+        <p>
+          
+        </p>
+      </div>
+    </div>
   );
 }
 
